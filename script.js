6132efed3b0db86b685dfaecbd6d0beb9020ccae
Ramon <ramonluciolinhares@gmail.com>
2020-07-25 18:48:52 -0300

    Set up a 'hover' effect

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ for (let i = 0; i < 256; i++) {
     let square = document.createElement('div');
     container.appendChild(square)
 }
+
+container.addEventListener('mouseover', e => {
+    e.target.classList.add('black')
+})
